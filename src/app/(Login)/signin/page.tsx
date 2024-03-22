16521b4e636c05a2eb594d7b0db30410253bfadd
Pubudu Kasun <pubudukasun289@gmail.com>
2024-03-23 03:01:48 +0530

    initial commit

--- src/app/(Login)/signin/page.tsx
+++ src/app/(Login)/signin/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import styless from "./signin.module.css";
 function page() {
   return (
-    <div>
-      <h1>Sign In page</h1>
+    <div className={styless.container}>
+      <div className={styless.headbar}></div>
+      <div>
+        <h1 className={styless.maintextArea}>
+          <span className={styless.admin}>Admin</span> Login
+        </h1>
+
+        <div className={styless.deranalogo}></div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1 className={styles.homn}>Home Page</h1>
     </>
   );
 }
