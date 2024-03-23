41f6404ea3c2716afbcd1679a62386f64e619ac6
Pubudu Kasun <pubudukasun289@gmail.com>
2024-03-23 16:49:43 +0530

    Login Page

--- src/app/(Login)/signin/page.tsx
+++ src/app/(Login)/signin/page.tsx
@@ -1,15 +1,48 @@
 import React from "react";
 import styless from "./signin.module.css";
+
 function page() {
   return (
     <div className={styless.container}>
+      <div className={styless.deranalogo}>{/* add a derana logo */}</div>
       <div className={styless.headbar}></div>
       <div>
         <h1 className={styless.maintextArea}>
           <span className={styless.admin}>Admin</span> Login
         </h1>
 
-        <div className={styless.deranalogo}></div>
+        <div className={styless.login_input_area}>
+          <p className={styless.login_desc}>
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit..."
+          </p>
+
+          <input
+            type="text"
+            placeholder="User Name"
+            className={styless.inputFields}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className={styless.inputFields}
+          />
+          <button className={styless.login_button}>Login</button>
+          <div>
+            <p className={styless.txtsignup}>
+              Don't have an account?{" "}
+              <span className={styless.signup_now}>
+                <a href="#">Sign Up Now</a>
+              </span>
+            </p>
+          </div>
+          <hr />
+          {/* social media links */}
+          <div>
+            <p className={styless.txtsignup}>Connect with social media</p>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
