0e57b7c1a7ea93368649d94a2bd72e46d343436b
Pubudu Kasun <pubudukasun289@gmail.com>
2024-03-31 04:05:42 +0530

    Create Artical Page

--- src/app/Components/latestnews.tsx
+++ src/app/Components/latestnews.tsx
@@ -4,26 +4,6 @@ import NewsImage from "../../../public/images/bnews.jpg";
 
 const latestnews = () => {
   return (
-    // <div className="grid grid-cols-1 md:auto-rows-[50px] gap-1 mx-2">
-    //   {data.map((item, i) => (
-    //     <div className="max-w-md rounded overflow-hidden shadow-lg  p-2">
-    //       <div className="flex">
-    //         <Image
-    //           className="flex"
-    //           src={NewsImage}
-    //           alt="img"
-    //           width={100}
-    //           height={100}
-    //         />
-    //         <div className="text-center p-1 shrink">
-    //           <h3 className="text-center">{item.title}</h3>
-    //           <p className="text-sm">{item.description}</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
-
     <div>
       {data.map((item, i) => (
         <div className="bg-white rounded-lg flex flex-row p-1 border-3 gap-1 mt-1 shadow transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300">
--- src/app/User/article/page.tsx
+++ src/app/User/article/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "../../Components/header";
 import Navbar from "../../Components/navbar";
 import Footer from "../../Components/footer";
+import LatestNews from "../../Components/latestnews";
+import Image from "next/image";
+import newsimg from ".././../../../public/images/bnews.jpg";
 function article_page() {
   return (
     <>
@@ -11,7 +14,49 @@ function article_page() {
         <Navbar />
       </section>
       <section>
-        <div>article section</div>
+        {/* All article section */}
+        <section>
+          <div className="max-w-6xl mx-auto grid md:grid-cols-12 p-4 gap-3">
+            <div className="md:col-span-9">
+              <div className="bg-cover">
+                <div className="bg-black"></div>
+                <h1 className="font-bold  text-white absolute p-3  ml-4 text-[15px]">
+                  #Article Head Line Design dcdsvf vfaff
+                </h1>
+                <Image
+                  src={newsimg}
+                  height={100}
+                  width={1000}
+                  alt="img"
+                  className="border-2 rounded-[10px] h-[250px]"
+                />
+              </div>
+              {/* details section */}
+              <div className="border-1 shadow mt-2 p-2 grid grid-cols-3 gap-4 divide-x">
+                <div className="p-1 ">
+                  <p className="text-[10px] ml-2">Publish By : 2024/12/12</p>
+                </div>
+                <div className="p-1 col-span-2">
+                  <p className="text-[10px] ml-2 ">Author : Alexander Crouso</p>
+                </div>
+              </div>
+              {/* artical desscription */}
+              <div className="texr-[12px] mt-3 p-2">
+                <p>hvdvv vadsvd vdsvadvd </p>
+              </div>
+            </div>
+            <div className=" rounded-[10px] sm:col-span-3">
+              <div className="border-2 rounded-[10px]">
+                <div className="bg-red-700 rounded-t-[10px]  p-2">
+                  <p className="text-white font-bold text-center">Lates News</p>
+                </div>
+                <div className="m-3 snap-y h-[500px] overflow-scroll no-scrollbar">
+                  <LatestNews />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
       <hr />
       <Footer />
